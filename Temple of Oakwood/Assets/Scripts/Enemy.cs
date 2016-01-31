0c97a027f48385bb2793ccee04f5d767c15b1fd2
Sultan Nakyp <sultannakyp@gmail.com>
2016-01-30 21:49:34 -0800

    Fixed spawning issues.

--- Temple of Oakwood/Assets/Scripts/Enemy.cs
+++ Temple of Oakwood/Assets/Scripts/Enemy.cs
@@ -4,14 +4,26 @@ using System.Collections;
 public class Enemy : MonoBehaviour {
 
 	public float moveSpeed = 2f;
+	public int HP = 2;					
 
 	// Use this for initialization
 	void Start () {
 
 	}
+
+	public void Damage()
+	{
+		// Reduce the number of hit points by one.
+		HP--;
+	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 		GetComponent<Rigidbody2D>().velocity = new Vector2(transform.localScale.x * moveSpeed, GetComponent<Rigidbody2D>().velocity.y);	
+
+		// If the enemy has zero or fewer hit points and isn't dead yet...
+		if(HP <= 0 /*&& !dead*/)
+			// ... call the death function.
+			Destroy(gameObject);
 	}
 }
