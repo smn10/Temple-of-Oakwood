1ab1ecdb4992d7b25d5b2905649299ee919eddc4
sonaleeshah <sssonalee@gmail.com>
2016-01-30 19:56:37 -0800

    Added script files for Enemy and Spawner

---
+++ Temple of Oakwood/Assets/Scripts/Enemy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	public float moveSpeed = 2f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Rigidbody2D>().velocity = new Vector2(transform.localScale.x * moveSpeed, GetComponent<Rigidbody2D>().velocity.y);	
+	}
+}
---
+++ Temple of Oakwood/Assets/Scripts/Spawner.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour
+{
+	public float spawnTime = 5f;		// The amount of time between each spawn.
+	public float spawnDelay = 3f;		// The amount of time before spawning starts.
+	public GameObject[] enemies;		// Array of enemy prefabs.
+	static int maxEnemyNum = 10;
+
+	void Start ()
+	{
+		// Start calling the Spawn function repeatedly after a delay .
+		InvokeRepeating("Spawn", spawnDelay, spawnTime);
+	}
+
+	void Spawn ()
+	{
+		// Instantiate a random enemy.
+		if (GameObject.FindGameObjectsWithTag ("Enemy").Length <= maxEnemyNum) {
+			int enemyIndex = Random.Range (0, enemies.Length);
+			Instantiate (enemies [enemyIndex], spawnPosition (), transform.rotation);
+		}
+	}
+
+	Vector3 spawnPosition() 
+	{
+		float height = Random.Range (-4.5f, 4f);
+		return new Vector3 (-12.5f, height, -1f);
+	}
+}
